d4dc81815e0910111934d1716e9bf28421b679bc
miguelpel <pelleteratdeborde@yahoo.fr>
2019-01-29 17:07:04 +0200

    fixed options in server-express and build

--- src/server-express.js
+++ src/server-express.js
@@ -5,12 +5,6 @@ import bodyParser from 'body-parser';
 const server = express();
 const PORT = process.env.PORT || 5000;
 
-const options = {
-  logPath: null,
-  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:5000/superscriptdb'
-};
-
-
 server.use(bodyParser.json());
 
 let bot;
@@ -27,6 +21,8 @@ server.get('/superscript', (req, res) => {
 });
 
 const options = {
+  logPath: null,
+  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:5000/superscriptdb',
   factSystem: {
     clean: true,
   },
